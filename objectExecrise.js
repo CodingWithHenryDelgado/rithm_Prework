28fa0690e672a54dadc4fbc6bb471046dfb1d7b5
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-04-06 23:22:32 -0400

    Added object execrise

---
+++ objectExecrise.js
@@ -0,0 +1,12 @@
+let trial = {
+    firstName: "Henry",
+    lastName: "Delgado",
+    occupation: 12
+};
+
+trial.firstName;
+trial[lastName];
+
+trial.hobby = "Coding";
+
+delete trial.occupation;
